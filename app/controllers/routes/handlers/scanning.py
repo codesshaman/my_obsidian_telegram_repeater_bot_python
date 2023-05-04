6821fc2b5638e65092b71f71e857ae9f44433467
codesshaman <nicksemail@yandex.ru>
2023-05-04 14:11:10 +0300

    add folders scanner and folder create keyboard

--- app/controllers/routes/handlers/scanning.py
+++ app/controllers/routes/handlers/scanning.py
@@ -8,5 +8,23 @@ keyboard1.row('Ok', 'Bye')
 
 @bot.message_handler(commands=['scan'])
 def scan_handler(message):
-    folder = Scan.scan()
-    bot.send_message(message.chat.id, folder, parse_mode='HTML', reply_markup=keyboard1)
+    result = Scan.scan()
+    folders = Scan.check_folders()
+    keyboard = types.ReplyKeyboardMarkup(row_width=2)
+    # keyboard = types.InlineKeyboardMarkup(row_width=2)
+    if folders[0]:
+        print('f0')
+        keyboard.add(types.InlineKeyboardButton('создать папку повторений'))
+    if folders[1]:
+        print('f1')
+        keyboard.add(types.InlineKeyboardButton('создать папку категорий'))
+    if folders[2]:
+        print('f2')
+        keyboard.add(types.InlineKeyboardButton('создать папку списков'))
+    if folders[3]:
+        print('f3')
+        keyboard.add(types.InlineKeyboardButton('создать папку заметок'))
+    bot.send_message(message.chat.id,
+                     result,
+                     parse_mode='HTML',
+                     reply_markup=keyboard)
--- app/model/file_module/scann.py
+++ app/model/file_module/scann.py
@@ -1,25 +1,47 @@
-import os
+import os, re
 from model.config_parser import parse_folder
 
 
+def dirscan():
+    "Сканирование существующих папок"
+    folder = parse_folder()
+    result = os.scandir(folder)
+    directories = []
+    for item in result:
+        if item.is_dir():
+            directories.append(item)
+    return directories
+
+
 class Scan:
-    "Класс для функций, индексирующих файлы"
+    "Класс сканирования файловой системы"
     def __init__(self, uri):
         super().__init__()
 
     def scan():
-        "Сканирование разделов"
-        folder = parse_folder()
-        result = os.scandir(folder)
-        print("Сканирую папку " + folder)
+        "Тестирование файловой системы"
+        dir = dirscan()
+        repeats = True if 'repeats' in dir else False
+        cat = True if 'categories' in dir else False
+        lists = True if 'lists' in dir else False
+        notes = True if 'notes' in dir else False
+        repansw = ' ' if repeats else '\nпапка для <b>повторений</b>.............. (создать)'
+        repcat = ' ' if cat else '\nпапка для <b>категорий</b>................. (создать)'
+        replists = ' ' if lists else '\nпапка для <b>списков заметок</b>.... (создать)'
+        repnotes = ' ' if notes else '\nпапка для <b>заметок</b>.................... (создать)'
+        answer = 'Все директории в порядке' if repeats and cat and lists and notes else 'Отсутствуют следующие директории:'
+        return answer + repansw + repcat + replists + repnotes
 
-        return str(result)
-
-    def scan_section():
-        "Сканирование разделов"
-        text = "Сканирую папку"
-        return text
-
+    def check_folders():
+        "Проверка отсутствия необходимых папок"
+        dir = dirscan()
+        answer = []
+        answer.append(False) if 'repeats' in dir else answer.append(True)
+        answer.append(False) if 'categories' in dir else answer.append(True)
+        answer.append(False) if 'lists' in dir else answer.append(True)
+        answer.append(False) if 'notes' in dir else answer.append(True)
+        print(answer)
+        return answer
     def scan_subsection():
         "Сканирование разделов"
         text = "Сканирую папку"
