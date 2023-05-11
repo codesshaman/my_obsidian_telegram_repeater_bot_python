a06d0e9b5e302bc7db0c1af8e17ee54b106e6932
codesshaman <nicksemail@yandex.ru>
2023-05-11 22:53:41 +0300

    add work aiogram menu

--- app/controllers/routes/handlers/allmenu.py
+++ app/controllers/routes/handlers/allmenu.py
@@ -1,22 +1,43 @@
 # from controllers.routes.handlers import creation
 # from controllers.routes.handlers import overview
 # from controllers.routes.handlers import settings
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
 from views.all_buttons import Buttons
 from model.core.bot import bot, dp
 from aiogram import types
 
+#https://www.youtube.com/watch?v=D9RvmMxtOTI
 
 main_menu = Buttons.main_menu()
+b1 = KeyboardButton(main_menu[0])
+b2 = KeyboardButton(main_menu[1])
+b3 = KeyboardButton(main_menu[2])
+b4 = KeyboardButton(main_menu[3])
+markup = ReplyKeyboardMarkup(resize_keyboard=True)
+markup.add(b1).insert(b2).add(b3).insert(b4)
 
 
 @dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    await message.reply("Привет!\nЯ Эхо-бот от Skillbox!\nОтправь мне любое сообщение, а я тебе обязательно отвечу.")
+async def main_menu(message: types.Message):
+    await message.reply("Главное меню",  reply_markup=markup)
 
 
-@dp.message_handler()
-async def echo(message: types.Message):
-    await message.answer(message.text)
+async def main_menu_handler(message: types.Message):
+    try:
+        if message.text == main_menu[0]:
+            await message.answer("Просмотр заметок")
+        elif message.text == main_menu[1]:
+            await message.answer("Создание категории")
+        elif message.text == main_menu[2]:
+            await message.answer("Создание заметки")
+        elif message.text == main_menu[3]:
+            await message.answer("Настройки")
+    except Exception as e:
+        print(e)
+
+dp.register_message_handler(main_menu, commands=['start'])
+dp.register_message_handler(main_menu_handler)
+# dp.register_message_handler(main_menu_handler, commands=['start'])
 #
 # @bot.message_handler(commands=['start'])
 # def main_menu(message):
